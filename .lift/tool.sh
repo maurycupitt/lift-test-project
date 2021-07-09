190e80c442cb1200ab53b33b4f1daf0685a8c090
Maury Cupitt <maury@cupitt.com>
2021-07-09 09:48:29 -0400

    fix tool script

--- .lift/tool.sh
+++ .lift/tool.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 function run() {
-    for i in $(git ls-files | grep -v lift) ; do
-        if ! jq $i >/dev/null 2>/dev/null ; then
-            echo "[ { \"type\" : \"bad json\", \"file\" : \"$i\", \"line\": 1,\
+    for i in \$(git ls-files | grep -v lift) ; do
+        if ! jq \$i >/dev/null 2>/dev/null ; then
+            echo "[ { \"type\" : \"bad json\", \"file\" : \"\$i\", \"line\": 1,\
                       \"message\" : \"JSON is many things, but it is not this.\",\
                       \"details_url\": null } ]"
             exit 0
@@ -11,7 +11,7 @@ function run() {
     echo "[ ]" ; exit 0
 }
 
-[[ "$3" = "version" ]] && echo "1"
-[[ "$3" = "applicable" ]] && echo "true"
-[[ "$3" = "run" ]] && run
-[[ -z "$3" ]] && echo '{ "version" : 1, "name" : "json-verifier" }'
+[[ "\$3" = "version" ]] && echo "1"
+[[ "\$3" = "applicable" ]] && echo "true"
+[[ "\$3" = "run" ]] && run
+[[ -z "\$3" ]] && echo '{ "version" : 1, "name" : "json-verifier" }'
